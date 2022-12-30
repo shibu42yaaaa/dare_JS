4dee3a61cf201fde96ccc5918ad79fdb8d6b7901
shibu42yaaaa <shibu.4242git.formac@gmail.com>
2022-12-31 00:04:20 +0900

    htmlへの表示

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 // フォームの情報を取ってくる
 const form = document.getElementById("form");
 const input = document.getElementById("input")
+const ul = document.getElementById("ul");
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   console.log(input.value);
+  add();
 })
 
+function add() {
+    const li = document.createElement("li");
+    li.innerText = input.value;
+    li.classList.add("list-group-item");
+    ul.appendChild(li);
+    input.value = ("");
+}
