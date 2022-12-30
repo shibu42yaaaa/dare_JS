82f8993acb0bf2e7a27ff28c42b73e65a2be7d3e
shibu42yaaaa <shibu.4242git.formac@gmail.com>
2022-12-31 00:58:33 +0900

    TODOをローカルストレージへ保存・取得

--- index.js
+++ index.js
@@ -3,19 +3,47 @@ const form = document.getElementById("form");
 const input = document.getElementById("input")
 const ul = document.getElementById("ul");
 
+//ローカルストレージからデータを取得する
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if (todos) {
+  todos.forEach(todo => {
+    add(todo);
+  })
+}
+
+
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   console.log(input.value);
   add();
 })
 
-function add() {
+function add(todo) {
   let todoText = input.value;
+
+  if (todo) {
+    todoText = todo;
+  }
+  
   if (todoText) {
     const li = document.createElement("li");
     li.innerText = todoText;
     li.classList.add("list-group-item");
     ul.appendChild(li);
     input.value = ("");
+    saveData();
   } 
+}
+
+function saveData() {
+    //todoのデータを取得
+    const lists = document.querySelectorAll("li");
+    //配列の定義
+    let todos = [];
+    lists.forEach(list => {
+        todos.push(list.innerText);
+    });
+    //ローカルストレージへ保存
+    localStorage.setItem("todos", JSON.stringify(todos));
 }
