b04cc7b9b48f50f1ff32b588176ecbb26c3856cd
shibu42yaaaa <shibu.4242git.formac@gmail.com>
2022-12-31 00:18:59 +0900

    条件分岐の記述追加

--- index.js
+++ index.js
@@ -10,9 +10,12 @@ form.addEventListener("submit", function (event) {
 })
 
 function add() {
+  let todoText = input.value;
+  if (todoText.length > 0) {
     const li = document.createElement("li");
-    li.innerText = input.value;
+    li.innerText = todoText;
     li.classList.add("list-group-item");
     ul.appendChild(li);
     input.value = ("");
+  } 
 }
