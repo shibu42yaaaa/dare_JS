8bcbb7d5c27da596bd67cf855a59cdd039bb880e
shibu42yaaaa <shibu.4242git.formac@gmail.com>
2022-12-31 02:04:00 +0900

    complete

--- index.js
+++ index.js
@@ -23,19 +23,31 @@ function add(todo) {
   let todoText = input.value;
 
   if (todo) {
-    todoText = todo;
+    todoText = todo.text;
   }
   
   if (todoText) {
     const li = document.createElement("li");
+
     li.innerText = todoText;
-    li.classList.add("list-group-item");
+    li.classList.add("list-group-item")
+
+    if (todo && todo.completed) {
+      li.classList.add("text-decoration-line-through");
+    }
+
     // 右クリックした時に削除する
     li.addEventListener("contextmenu", function(event){
         //処理内容
           // デフォルトの右クリアクションを禁止する
         event.preventDefault();
         li.remove();
+        saveData();
+    });
+
+    // 左クリックで取り消し線をつける
+    li.addEventListener("click", function () {
+        li.classList.toggle("text-decoration-line-through");
         saveData();
     });
 
@@ -49,9 +61,14 @@ function saveData() {
     //todoのデータを取得
     const lists = document.querySelectorAll("li");
     //配列の定義
-    let todos = [];
-    lists.forEach(list => {
-        todos.push(list.innerText);
+    const todos = [];
+
+    lists.forEach((li) => {
+        // 完了状態も合わせてもつ
+      todos.push({
+        text: li.innerText,
+        completed: li.classList.contains("text-decoration-line-through"),
+      });
     });
     //ローカルストレージへ保存
     localStorage.setItem("todos", JSON.stringify(todos));
