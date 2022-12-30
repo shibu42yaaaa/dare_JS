cad085888a0c83c239022e9acb8f15cc9ab30f11
shibu42yaaaa <shibu.4242git.formac@gmail.com>
2022-12-30 16:35:53 +0900

    フォームの値を取得

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hello World');
+// フォームの情報を取ってくる
+const form = document.getElementById("form");
+const input = document.getElementById("input")
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  console.log(input.value);
+})
+
